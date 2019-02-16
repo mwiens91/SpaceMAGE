3cc7f15b9206088761c89915550d5160ee5cffb3
Matt Wiens <mwiens91@gmail.com>
2019-02-15 23:39:43 -0800

    Add table with music tracks

--- game/title_menu.lua
+++ game/title_menu.lua
@@ -1,9 +1,16 @@
 local title_menu = {
+  music_playing = false,
 }
 
 -- The title screen
 function title_menu:update(dt)
   debug_text = "TITLE MENU"
+
+  -- Start the music
+  if not title_menu["music_playing"] then
+    music["title"]:setLooping(true)
+    music["title"]:play()
+  end
 end
 
 return title_menu
--- main.lua
+++ main.lua
@@ -31,6 +31,10 @@ function love.load()
   -- TODO add more states
   current_state = "title_menu"
 
+  -- Build up a table of music tracks
+  music = {}
+  music["title"] = love.audio.newSource("media/audio/ambient01.ogg", "stream")
+
   -- DEBUG some text
   debug_text = "HEY"
 end

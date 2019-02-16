e5496a618b236f0e1e5403b3f4341befd3b050bd
Branko Bajcetic <bajcetic.branko@gmail.com>
2019-02-16 12:12:52 -0800

    Make enemy ship shoot at main ship

--- game/enemies.lua
+++ game/enemies.lua
@@ -31,14 +31,26 @@ end
 
 function enemies.shoot(enemy)
   main_x, main_y = ship.get_origin()
-  local diff_x = main_x - enemy.x
-  local diff_y = main_y - enemy.y
+  local diff_x = (main_x - enemy.x)
+  local diff_y = (main_y - enemy.y)
+  local diff_lastx = enemy.main_lastx - enemy.x
+  local diff_lasty = enemy.main_lasty - enemy.y
+  local last_rotation = math.atan(diff_lasty/diff_lastx)
   local rotation = math.atan(diff_y/diff_x)
   if diff_x < 0 then
     rotation = rotation + math.pi
   end
+  if diff_lastx < 0 then
+    last_rotation = last_rotation + math.pi
+  end
+  local new_rotation = rotation + (rotation-last_rotation)*0.5
+  --rotation = rotation + rotation-last_rotation
   --rotation = rotation-enemies.shot_error/2 + math.random()*enemies.shot_error
-  projectiles.projectile_init(enemy.x, enemy.y, rotation, enemy.shot_speed)
+  --projectiles.projectile_init(enemy.x, enemy.y, rotation, enemy.shot_speed)
+  --projectiles.projectile_init(enemy.x, enemy.y, last_rotation, enemy.shot_speed)
+  projectiles.projectile_init(enemy.x, enemy.y, new_rotation, enemy.shot_speed)
+  enemy.main_lastx = main_x
+  enemy.main_lasty = main_y
 end
 
 function enemies.enemy_init(x, y)
@@ -50,6 +62,8 @@ function enemies.enemy_init(x, y)
   enemy_ship.shot_speed = 10
   enemy_ship.shot_delay = 1
   enemy_ship.last_shot = 0
+  enemy_ship.main_lastx = 30
+  enemy_ship.main_lasty = GAME_HEIGHT/2
   table.insert(enemy_ships, enemy_ship)
 end
 

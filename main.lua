4977a792d93a04308692f224cf5059faa927da00
Matt Wiens <mwiens91@gmail.com>
2019-02-15 21:56:22 -0800

    Move push stuff to load function

--- main.lua
+++ main.lua
@@ -2,21 +2,23 @@ lume = require "lib.lume.lume"
 push = require "lib.push.push"
 suit = require "lib.suit"
 
-local gameWidth, gameHeight = 1280, 720
-local windowWidth, windowHeight = love.window.getDesktopDimensions()
-
-push:setupScreen(
-  gameWidth,
-  gameHeight,
-  windowWidth,
-  windowHeight,
-  {
-    fullscreen = true,
-    resizable = true
-  }
-)
-
 function love.load()
+  -- Setup screen resolution and upscaling with push
+  local game_width, game_height = 1280, 720
+  local window_width, window_height = love.window.getDesktopDimensions()
+
+  push:setupScreen(
+    game_width,
+    game_height,
+    window_width,
+    window_height,
+    {
+      fullscreen = true,
+      resizable = true
+    }
+  )
+
+  -- Other setup here
 end
 
 function love.update(dt)

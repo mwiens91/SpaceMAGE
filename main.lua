39c636436008841eaf44970464d373faa38f5fe6
Matt Wiens <mwiens91@gmail.com>
2019-02-15 21:44:27 -0800

    Add some test text

--- main.lua
+++ main.lua
@@ -1,3 +1,31 @@
 lume = require "lib.lume.lume"
 push = require "lib.push.push"
 suit = require "lib.suit"
+
+local gameWidth, gameHeight = 1280, 720
+local windowWidth, windowHeight = love.window.getDesktopDimensions()
+
+push:setupScreen(
+  gameWidth,
+  gameHeight,
+  windowWidth,
+  windowHeight,
+  {
+    fullscreen = false,
+    resizable = true
+  }
+)
+
+function love.load()
+end
+
+function love.update(dt)
+end
+
+function love.draw()
+  push:start()
+
+  love.graphics.print("HKLSDFKLDF", 1200, 700)
+
+  push:finish()
+end

17f7c61ab4e2bdb10e0c208e15f9e511b38769bd
Branko Bajcetic <bajcetic.branko@gmail.com>
2019-02-17 14:36:32 -0800

    Make missiles slower than main ship

--- game/projectiles.lua
+++ game/projectiles.lua
@@ -2,7 +2,7 @@ local projectiles = {
   is_loaded = false,
   images = {"media/img/projectile1.png", "media/img/projectile2.png",},
   p_damage = {5, 15,},
-  p_speed = {10, 4},
+  p_speed = {10, 3},
 
 }
 

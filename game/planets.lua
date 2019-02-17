0c1a546037f513283b68d9983b4b50c5c317242a
Branko Bajcetic <bajcetic.branko@gmail.com>
2019-02-17 05:15:51 -0800

    Change planet's sprite to be an attribute of the planet object/table instead of just a global variable

--- game/planets.lua
+++ game/planets.lua
@@ -11,10 +11,9 @@ function planets.load(planet_index)
     planet.x = GAME_WIDTH
     planet.y = GAME_HEIGHT/2
     local img_file = planets.images[planet.index]
-    planet_sprite = love.graphics.newImage(img_file)
-    --planet_sprite = love.graphics.newImage("media/img/planet1.png")
-    planet.width = planet_sprite:getWidth()
-    planet.height = planet_sprite:getHeight()
+    planet.sprite = love.graphics.newImage(img_file)
+    planet.width = planet.sprite:getWidth()
+    planet.height = planet.sprite:getHeight()
     planets.is_loaded = true
   end
 end
@@ -24,7 +23,7 @@ function planets.get_origin(planet)
 end
 
 function planets.draw()
-  love.graphics.draw(planet_sprite, planet.x, planet.y, 0, 1, 1, planet.width/2, planet.height/2)
+  love.graphics.draw(planet.sprite, planet.x, planet.y, 0, 1, 1, planet.width/2, planet.height/2)
 end
 
 

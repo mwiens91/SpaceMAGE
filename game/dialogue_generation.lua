03acf589791750878220f76efb5a9d025aab828c
Matt Wiens <mwiens91@gmail.com>
2019-02-16 21:35:39 -0800

    Add more drone death dialogue options

--- game/dialogue_generation.lua
+++ game/dialogue_generation.lua
@@ -9,8 +9,26 @@ end
 
 
 -- Generate a death message
-function dialogue_generation.fealty_announcement(name)
-  return name ..  " has fallen."
+function dialogue_generation.death_announcement(name, drone_type)
+  if drone_type == ATTACK_TYPE then
+    return name .. lume.weightedchoice({
+      [" has fallen in battle."] = 70,
+      [" has been exterminated."] = 15,
+      [" has been decommissioned."] = 8,
+      [" has defected."] = 2
+    })
+  elseif drone_type == EXPLORE_TYPE then
+    return name .. lume.weightedchoice({
+      [" has stopped transmitting."] = 60,
+      [" has defected."] = 20,
+      [" has been decommissioned."] = 20
+    })
+  else
+    return name .. lume.weightedchoice({
+      [" has deserted."] = 80,
+      [" have disconnected."] = 20,
+    })
+  end
 end
 
 

9f39862bbcdd9f1b428a057be827ada4f0c037ab
Branko Bajcetic <bajcetic.branko@gmail.com>
2019-02-17 14:17:50 -0800

    Add collision detection for stasis

--- game/collision.lua
+++ game/collision.lua
@@ -33,6 +33,7 @@ function collision.check_main(proj_index)
 end
 
 function collision.check_reflector(proj_index)
+  the_proj = all_projectiles[proj_index]
   leftx1, rightx1, topy1, bottomy1, leftx2, rightx2, topy2, bottomy2 = weapons.reflector.get_hit_box()
   proj_leftx, proj_rightx, proj_topy, proj_bottomy = projectiles.get_hit_box(proj_index)
   cond1 = leftx1 <= proj_rightx
@@ -44,7 +45,6 @@ function collision.check_reflector(proj_index)
   cond7 = topy2 <= proj_bottomy
   cond8 = proj_topy <= bottomy2
   if ((cond1 and cond2 and cond3 and cond4) or (cond5 and cond6 and cond7 and cond8)) then
-    the_proj = all_projectiles[proj_index]
     print("SHIELD HIT")
     weapons.reflector.got_hit(the_proj.damage)
     if(the_proj.p_type == 1) then
@@ -59,11 +59,32 @@ function collision.check_reflector(proj_index)
   return false
 end
 
+function collision.check_stasis(proj_index)
+  the_proj = all_projectiles[proj_index]
+  leftx1, rightx1, topy1, bottomy1 = weapons.stasis.get_hit_box()
+  proj_leftx, proj_rightx, proj_topy, proj_bottomy = projectiles.get_hit_box(proj_index)
+  cond1 = leftx1 <= proj_rightx
+  cond2 = proj_leftx <= rightx1
+  cond3 = topy1 <= proj_bottomy
+  cond4 = proj_topy <= bottomy1
+  if (cond1 and cond2 and cond3 and cond4) then
+    if(the_proj.p_type == 1) then
+      weapons.stasis.got_hit()
+      table.remove(all_projectiles, proj_index)
+    end
+    return true
+  end
+  return false
+end
+
 function collision.collision_detection()
   proj_hit = false
   for i, proj in ipairs(all_projectiles) do
     if not proj_hit and weapons.reflector.deployed then
       proj_hit = collision.check_reflector(i)
+    end
+    if not proj_hit and weapons.stasis.deployed then
+      proj_hit = collision.check_stasis(i)
     end
     if not proj_hit then
       proj_hit = collision.check_main(i)
